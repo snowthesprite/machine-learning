f49fbda92f1b4e3ffac24f55bcfa577daf73c433
snowthesprite <mpapaya9@gmail.com>
2020-09-07 21:18:59 +0000

    Finished matrix multiply

--- src/matrix.py
+++ src/matrix.py
@@ -29,14 +29,11 @@ class Matrix :
         return Matrix(new_elements)
 
     def matrix_multiply(self, matrix) :
-        new_elements = [[],[]]
-        outer_leng = 0
-        while outer_leng < 2 :
-            inner_leng = 0
-            while inner_leng < 2 : 
-                new_elements[outer_leng].append(self.elements[outer_leng][inner_leng] * matrix.elements[outer_leng][inner_leng])
-                inner_leng += 1
-            outer_leng += 1
+        top_left_elements = self.elements[0][0] * matrix.elements[0][0] + self.elements[0][1] * matrix.elements[1][0]
+        top_right_elements = self.elements[0][0] * matrix.elements[0][1] + self.elements[0][1] * matrix.elements[1][1]
+        bottom_left_elements = self.elements[1][0] * matrix.elements[0][0] + self.elements[1][1] * matrix.elements[1][0]
+        bottom_right_elements = self.elements[1][0] * matrix.elements[0][1] + self.elements[1][1] * matrix.elements[1][1]
+        new_elements = [[top_left_elements, top_right_elements], [bottom_left_elements, bottom_right_elements]]
         return Matrix(new_elements)
         
     def scalar_multiply(self, scalar) :
@@ -45,7 +42,6 @@ class Matrix :
         while outer_leng < 2 :
             inner_leng = 0
             while inner_leng < 2 : 
-                
                 new_elements[outer_leng].append(self.elements[outer_leng][inner_leng] * scalar)
                 inner_leng += 1
             outer_leng += 1
