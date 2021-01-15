dc7f8a15056c885c9284c1acfaf27fbe7aea48a2
snowthesprite <mpapaya9@gmail.com>
2021-01-15 01:33:00 +0000

    Forgot about rand_optimizer Added helper functs for calc cost simplified diagonal test testing and debugging currently

--- analysis/8_queens.py
+++ analysis/8_queens.py
@@ -1,3 +1,4 @@
+import random
 def show_board(locations) :
     board = [['.' for _ in range(8)] for __ in range(8)]
     index = 0
@@ -12,60 +13,63 @@ def show_board(locations) :
         row_string = '  '.join(row_array)
         print(row_string)
 
+def on_col(point_1, point_2) :
+    test_point = [0, point_1[1]]
+    while test_point[0] < 8 :
+        if tuple(test_point) == point_2 :
+            return True
+        test_point[0] += 1
+    return False
+
+def on_row(point_1, point_2) :
+    test_point = [point_1[0], 0]
+    while test_point[0] < 8 :
+        if tuple(test_point) == point_2 :
+            return True
+        test_point[1] += 1
+    return False
+
+def on_diagonal(point_1, point_2) :
+    slope = (point_2[1] - point_1[1]) / (point_2[0] - point_1[1])
+    if slope == 1 or slope == -1 :
+        return True
+    return False
+
 def calc_cost(locations) : 
     hits = 0
     for queen in locations :
-        for direction in range(5) :
-            for opposing_queen in locations :
-                if direction == 0 : #Up and Down
-                    test_point = [0, queen[1]]
-                    while test_point[0] < 8 :
-                        if tuple(test_point) == opposing_queen and opposing_queen != queen :
-                            hits += 1
-                        test_point[0] += 1
-                elif direction == 2 : #Left and right
-                    test_point = [queen[0], 0]
-                    while test_point[1] < 8 :
-                        if tuple(test_point) == opposing_queen and opposing_queen != queen :
-                            hits += 1
-                        test_point[1] += 1
-                elif direction == 3 : #top left to bottom right
-                    test_point = list(queen)
-                    while test_point[0] > 0 and test_point[1] > 0 : #top left
-                        test_point[0] -= 1
-                        test_point[1] -= 1
-                        if tuple(test_point) == opposing_queen :
-                            hits += 1
-                    test_point = list(queen)
-                    while test_point[0] < 8 and test_point[1] < 8 : #bottom right
-                        test_point[0] += 1
-                        test_point[1] += 1
-                        if tuple(test_point) == opposing_queen :
-                            hits += 1
-                elif direction == 4 : #top right to bottom left
-                    test_point = list(queen)
-                    while test_point[0] > 0 and test_point[1] < 8 : #top right
-                        test_point[0] -= 1
-                        test_point[1] += 1                            
-                        if tuple(test_point) == opposing_queen :
-                            hits += 1
-                    test_point = list(queen)
-                    while test_point[0] < 8 and test_point[1] > 0 : #bottom left
-                        test_point[0] += 1
-                        test_point[1] -= 1
-                        if tuple(test_point) == opposing_queen :
-                            hits += 1
+        for opposing_queen in locations :
+            if queen != opposing_queen :
+                row_test = on_row(queen, opposing_queen)
+                col_test = on_col(queen, opposing_queen)
+                diagonal_test = on_diagonal(queen, opposing_queen)
+                if row_test or col_test or diagonal_test :
+                    hits += 1
     return hits / 2
 
-
+def random_optimizer(n) :
+    all_locations = [[] for _ in range(n)]
+    for location_index in range(n) :
+        for queen in range(8) :
+            queen_location = (random.randint(0,8), random.randint(0,8))
+            all_locations[location_index].append(queen_location)
+    lowest_cost = calc_cost(all_locations[0])
+    for location_set in all_locations :
+        cost = calc_cost(location_set)
+        if cost <= lowest_cost :
+            lowest_cost = cost
+            lowest_location = location_set
+    return {'locations' : lowest_location, 'cost' : lowest_cost}
                        
-
 
 locations = [(0,0), (6,1), (2,2), (5,3), (4,4), (7,5), (1,6), (2,6)]
 print('The board')
 print()
 show_board(locations)
 print()
+
 print('Can calc_cost find all overlaps between queens?')
 assert calc_cost(locations) == 10, 'No it can not'
 print('Yes it can')
+
+print(random_optimizer(5))
