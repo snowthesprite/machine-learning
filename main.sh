3de21803805f8e791a0152ede96099e87dbbe609
snowthesprite <mpapaya9@gmail.com>
2021-01-18 23:08:22 +0000

    Answered the Overleaf and added asserts Assignment Finished

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #python tests/test_matrix.py
-python tests/test_dataframe.py
-#python tests/test_linear_regressor.py
+#python tests/test_dataframe.py
+python tests/test_linear_regressor.py
 #python tests/test_gradient_descent.py
 
 #python analysis/8_queens.py
--- tests/test_linear_regressor.py
+++ tests/test_linear_regressor.py
@@ -53,4 +53,7 @@ df = df.create_interaction_terms('beef', 'pb')
 assert df.columns == ['beef', 'pb', 'rating', 'beef * pb']
 assert df.to_array() == [[0, 0, 1, 0],  [1, 0, 2, 0],  [2, 0, 4, 0],  [4, 0, 8, 0],  [6, 0, 9, 0],  [0, 2, 2, 0],  [0, 4, 5, 0],  [0, 6, 7, 0],  [0, 8, 6, 0], [2, 2, 0, 4], [3, 4, 0, 12]]
 
-print(df.predict)
+regressor = LinearRegressor(df, 'rating')
+print(regressor.coefficients)
+print(regressor.predict({'beef' : 5, 'pb' : 5, 'beef * pb' : 25}))
+print(regressor.predict({'beef' : 5, 'pb' : 0}))
