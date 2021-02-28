ce974940d0c652cb68b8d492434d0cae972398e2
snowthesprite <mpapaya9@gmail.com>
2021-02-28 00:27:35 +0000

    Plotted the regressors

--- analysis/rocket_takeoff_regression.py
+++ analysis/rocket_takeoff_regression.py
@@ -26,3 +26,23 @@ print(cube_regressor.coefficients)
 print('5 seconds:', cube_regressor.predict({'time' : 5}))
 print('10 seconds:', cube_regressor.predict({'time' : 10}))
 print('200 seconds:', cube_regressor.predict({'time' : 200}), "\n")
+'''
+import matplotlib.pyplot as plt
+
+plt.style.use('bmh')
+
+quad_y = []
+x_points = [x for x in range(200)]
+cube_y = []
+
+for x in x_points :
+    quad_y.append(quad_regressor.predict({'time' : x}))
+    cube_y.append(cube_regressor.predict({'time' : x}))
+
+plt.plot(x_points, quad_y)
+plt.plot(x_points, cube_y)
+plt.xlabel('Time in seconds')
+plt.ylabel('Distance from ground')
+plt.legend(['Quadratic', 'Cubic'])
+plt.savefig('analysis/rocketman.png')
+'''
