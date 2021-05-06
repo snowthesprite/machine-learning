c92b2f81aac28b2eb4ddb300dde2e95a25d5e06f
snowthesprite <>
2021-05-06 21:08:54 +0000

    Finished compacting Remember to delete that home column, you dribbling buffoon Assign 88 Fin

--- analysis/KNN/KNN_modeling_normal.py
+++ analysis/KNN/KNN_modeling_normal.py
@@ -1,10 +1,7 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
-from timeit import default_timer as timer
-start_time = timer()
 
 df = pd.read_csv('/home/runner/machine-learning/analysis/KNN/book_type.csv')
-
 
 def change_type_to_int (book_type) :
     if book_type == "children's book" :
@@ -13,110 +10,79 @@ def change_type_to_int (book_type) :
         return 1
 
 df['book type'] = df['book type'].apply(change_type_to_int)
-
 book_type = df['book type']
-
 del df['book type']
+
+columns = list(df.columns)
 
 def find_accuracy (predict, actual) :
     num_correct = 0
-    num_incorrect = 0
     for i in range(len(predict)) :
         if predict[i] == actual[i] :
             num_correct += 1
-        else :
-            num_incorrect+=1 
-    return num_correct/(num_correct + num_incorrect)
+    return num_correct/len(predict)
 
-##Unscaled
-#'''
+k_vals = range(1,100,2)
+
+df_simp = df.copy()
+df_min_max = df.copy()
+df_z = df.copy()
+
+for column in columns :
+    df_simp[column] = df[column]/df[column].max()
+
+    df_min_max[column] = (df[column]-df[column].min())/(df[column].max() - df[column].min())
+
+    df_z[column] = (df[column]-df[column].mean())/(df[column].std())
+
 acc_un = []
-predict_un = {k : [] for k in range(1,100,2)}
-#'''
-
-##Simple scaling
-#'''
-df_simple = df.copy()
-df_simple['num pages'] = df['num pages']/df['num pages'].max()
-df_simple['num unique words'] = df['num unique words']/df['num unique words'].max()
-df_simple['avg sentence length'] = df['avg sentence length']/df['avg sentence length'].max()
-df_simple['avg word size'] = df['avg word size']/df['avg word size'].max()
+predict_un = {k : [] for k in k_vals}
 
 acc_simp = []
-predict_simp = {k : [] for k in range(1,100,2)}
-#'''
-
-##Min-max
-#'''
-df_min_max = df.copy()
-df_min_max['num pages'] = (df['num pages']-df['num pages'].min())/(df['num pages'].max() - df['num pages'].min())
-df_min_max['num unique words'] = (df['num unique words']-df['num unique words'].min())/(df['num unique words'].max() - df['num unique words'].min())
-df_min_max['avg sentence length'] = (df['avg sentence length']-df['avg sentence length'].min())/(df['avg sentence length'].max() - df['avg sentence length'].min())
-df_min_max['avg word size'] = (df['avg word size']-df['avg word size'].min())/(df['avg word size'].max() - df['avg word size'].min())
+predict_simp = {k : [] for k in k_vals}
 
 acc_min_max = []
-predict_min_max = {k : [] for k in range(1,100,2)}
-#'''
-
-##z-score
-#'''
-df_z = df.copy()
-df_z['num pages'] = (df['num pages']-df['num pages'].mean())/(df['num pages'].std())
-df_z['num unique words'] = (df['num unique words']-df['num unique words'].mean())/(df['num unique words'].std())
-df_z['avg sentence length'] = (df['avg sentence length']-df['avg sentence length'].mean())/(df['avg sentence length'].std())
-df_z['avg word size'] = (df['avg word size']-df['avg word size'].mean())/(df['avg word size'].std())
+predict_min_max = {k : [] for k in k_vals}
 
 acc_z = []
-predict_z = {k : [] for k in range(1,100,2)}
-#'''
-#'''
+predict_z = {k : [] for k in k_vals}
+
 for row_id in range(book_type.size) :
     mod_df = df.drop(row_id)
-    mod_df_simp = df_simple.drop(row_id)
+    mod_df_simp = df_simp.drop(row_id)
     mod_df_min = df_min_max.drop(row_id)
     mod_df_z = df_z.drop(row_id)
 
     mod_book_type = book_type.drop(row_id)
-    for k in range(1,100,2) :
+    for k in k_vals :
         KNN = KNeighborsClassifier(n_neighbors=k)
-        #'''
+
         KNN.fit(mod_df, mod_book_type)
         predict_un[k].append(KNN.predict([df.iloc[row_id]]))
-        #'''
-        #'''
+
         KNN.fit(mod_df_simp, mod_book_type)
-        predict_simp[k].append(KNN.predict([df_simple.iloc[row_id]]))
-        #'''
-        #'''
+        predict_simp[k].append(KNN.predict([df_simp.iloc[row_id]]))
+
         KNN.fit(mod_df_min, mod_book_type)
         predict_min_max[k].append(KNN.predict([df_min_max.iloc[row_id]]))
-        #'''
-        #'''
+
         KNN.fit(mod_df_z, mod_book_type)
         predict_z[k].append(KNN.predict([df_z.iloc[row_id]]))
-        #'''
-    if row_id % 10 == 0 :
-        print('row', row_id)
 
-#'''
-
-for k in range(1,100,2) :
+for k in k_vals :
     acc_un.append(find_accuracy(predict_un[k],book_type))
     acc_simp.append(find_accuracy(predict_simp[k],book_type))
     acc_min_max.append(find_accuracy(predict_min_max[k],book_type))
     acc_z.append(find_accuracy(predict_z[k],book_type))
 
-#'''
 import matplotlib.pyplot as plt
-#plt.style.use('bmh')
 
-plt.plot(range(1,100,2), acc_un)
-plt.plot(range(1,100,2), acc_simp)
-plt.plot(range(1,100,2), acc_min_max)
-plt.plot(range(1,100,2), acc_z)
-
+plt.plot(k_vals, acc_un)
+plt.plot(k_vals, acc_simp)
+plt.plot(k_vals, acc_min_max)
+plt.plot(k_vals, acc_z)
+plt.xlable('k')
+plt.ylable('Accuracy')
 plt.legend(['Unaltered', 'Simple', 'Min-Max', 'Z'])
 
-plt.savefig('analysis/KNN/KNN_simp.png')
-
-print('\n\n', timer() - start_time, 'sec \n\n\n')
+plt.savefig('analysis/KNN/KNN_model.png')
