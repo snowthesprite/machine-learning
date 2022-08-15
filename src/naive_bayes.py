e8372c319066f4ef3bbc28f3c81980a2467dea2a
NamelessEarth <mpapaya9@gmail.com>
2022-08-15 21:05:51 +0000

    Finished Naive Bayes

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -2,7 +2,8 @@ class NaiveBayes () :
     def __init__(self, data) :
         self.data = data
         self.classes = ['scam', 'not']
-        self.var = data.keys().remove('class')
+        self.var = list(data.keys())
+        self.var.remove('class')
         self.total =  len(data['class'])
         self.class_percent = self.calc_class_percent()
         self.var_class_percent = self.calc_var_class_percent()
@@ -19,12 +20,15 @@ class NaiveBayes () :
 
 
     def calc_var_class_percent(self) : 
-        kind_totals = {kind: self.class_percent[kind]*self.total}
-        var_class_percent = {kind : {var: {1:0} for var in self.var} for kind in self.kind}
+        kind_totals = {kind: self.class_percent[kind]*self.total for kind in self.classes}
+        var_class_percent = {}
+        for kind in self.classes :
+            var_class_percent[kind] = {var : {1:0, 0:0} for var in self.var}
+        print(kind_totals)
         for col in range(self.total) :
-            kind = data['class'][col]
+            kind = self.data['class'][col]
             for var in self.var :
-                var_class_percent[kind][var][data[var][col]] += 1/kind_totals[kind]
+                var_class_percent[kind][var][self.data[var][col]] += 1/kind_totals[kind]
 
         return var_class_percent
 
@@ -37,5 +41,13 @@ class NaiveBayes () :
         return class_chance
 
 
-    def find_class(self, imputs) :
-        pass
+    def find_class(self, imput) :
+        chances = self.chance_of_class(imput)
+        #print(chances)
+        class_kind = None
+        class_chance = 0
+        for (kind, percent) in chances.items() :
+            if percent > class_chance :
+                class_chance = percent
+                class_kind = kind
+        return class_kind
--- tests/test_naive_bayes.py
+++ tests/test_naive_bayes.py
@@ -2,9 +2,28 @@ import sys
 sys.path.append('src')
 from naive_bayes import NaiveBayes
 
-data = {'class': [0,1,1,0,0,1,0,0,1,0],
+data = {'class': ['not','scam','scam','not','not','scam','not','not','scam','not'],
         'errors': [0,1,1,0,0,1,1,0,1,0],
         'links': [0,1,1,0,1,1,0,1,0,1]}
 
-imput = {'errors': [0,1,1,0],
-        'links': [0,1,0,1]}
+bayes = NaiveBayes(data)
+
+imput = {'errors': 0,
+        'links': 0}
+
+print(bayes.find_class(imput))
+
+imput = {'errors': 1,
+        'links': 1}
+
+print(bayes.find_class(imput))
+
+imput = {'errors': 1,
+        'links': 0}
+
+print(bayes.find_class(imput))
+
+imput = {'errors': 0,
+        'links': 1}
+
+print(bayes.find_class(imput))
